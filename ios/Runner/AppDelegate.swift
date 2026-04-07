1d8fd5207639a6f387eefa79a15497d5931476a2
emmanuel <e.amos@systoneit.com>
2026-04-07 08:58:12 +0100

    rebuild 2

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,21 +1,17 @@
 import Firebase
 import UIKit
+import Flutter
 import flutter_local_notifications
 import app_links
 
-@UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
+@main // Use @main instead of @UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+  
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     FirebaseApp.configure()
-
-    GeneratedPluginRegistrant.register(with: self)
-
-    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
-      GeneratedPluginRegistrant.register(with: registry)
-    }
 
     // Set the notification delegate
     UNUserNotificationCenter.current().delegate = self
@@ -28,8 +24,22 @@ import app_links
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
+  // NEW: Modern Flutter plugin registration lives here now
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+    
+    // Specifically for local notifications background handling
+    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+      GeneratedPluginRegistrant.register(with: registry)
+    }
+  }
+
   // Handle foreground notifications
-  override func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+  override func userNotificationCenter(
+    _ center: UNUserNotificationCenter, 
+    willPresent notification: UNNotification, 
+    withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+  ) {
     completionHandler([.alert, .badge, .sound])
   }
 }
